fc026c207f51a2562d8be602555c71929f5529ac
NinaHahne <ninamariehahne@gmail.com>
2026-03-16 19:42:19 +0100

    chore: deploy setup / appId

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,13 +3,14 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: '4va3j34l',
-    dataset: 'production'
+    dataset: 'production',
   },
   deployment: {
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
+    appId: 'ut1658avou0tjaf4x1d50rft',
     autoUpdates: true,
-  }
+  },
 })
